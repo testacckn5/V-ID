e36ef3296fe024aed60d8d6c7782fee98790e765
testacckn5 <testacckn5@gmail.com>
2026-02-03 17:58:09 -0500

    Optimize Service Worker implementation

--- public/uv/uv.sw.js
+++ public/uv/uv.sw.js
@@ -1,10 +1,9 @@
-/* This is the missing Service Worker script */
+/* Optimized Service Worker */
 importScripts('./uv.bundle.js');
 importScripts('./uv.config.js');
-importScripts('./uv.sw.js'); 
 
-const sw = new UVServiceWorker();
+const uvService = new UVServiceWorker();
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(sw.fetch(event));
+    event.respondWith(uvService.fetch(event));
 });
